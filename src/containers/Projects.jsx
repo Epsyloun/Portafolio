d304dddd6cdb87034521170a7eb5eb6e7522c6b3
Rodrigo Diaz <rodrigo.diaz8b@gmail.com>
2022-10-07 13:54:33 -0600

    [ADD] se añadieron grid de projects en mobile

--- src/containers/Projects.jsx
+++ src/containers/Projects.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import me_large from '@images/Rodrigo.jpeg'
+import LanguageIcon from '@mui/icons-material/Language';
+import PaletteIcon from '@mui/icons-material/Palette';
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
 
 function Projects() {
+  const largeIcon = {fontSize:'3em', textAlign:'center'}
   return (
   <section id='Projects' className='projects'>
+    <div className='projects__title'>
+      <h2>Projects</h2>
+      <p>Projects category</p>
+    </div>
+    <div className='projects__web projects-category'>
+      <LanguageIcon sx={largeIcon}/>
+      <h4>Web Projects</h4>
+    </div>
+    <div className='projects__design projects-category'>
+      <PaletteIcon sx={largeIcon}/>
+      <h4>UI/UX Projects</h4>
+    </div>
+    <div className='projects__mobile projects-category'>
+      <PhoneAndroidIcon sx={largeIcon}/>
+      <h4>Mobile Projects</h4>
+    </div>
   </section>
   );
 }
