57f56b485009c48396f810454d694ede1299f1b0
Rodrigo Diaz <rodrigo.diaz8b@gmail.com>
2022-10-14 13:50:59 -0600

    [ADD] terminando parte de proyecto web y añadiendo responsividad

--- src/components/ProjectSlide.jsx
+++ src/components/ProjectSlide.jsx
@@ -1,9 +1,20 @@
 import React, { useState,useEffect } from 'react';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
-function ProjectSlide({num}) {
+function ProjectSlide({num, image, icon , title, desc,link, git}) {
+  function OpenProject(){
+    window.open(link, '_blank');
+  }
   return (
-    <div className={`modal__slide slide__${num}`}>
-      <div className='slide__content'></div>
+    <div id="#modal" className={`modal__slide slide__${num}`}>
+      <div className='slide__content'>
+        {icon}
+        <h2 className='slide__title'>{title}</h2>
+        <p className='slide__desc'>{desc}</p>
+        <button onClick={OpenProject} className='slide__button'>View</button>
+        <a className='slide__git' href={git} target="_blank"><GitHubIcon sx={{fontSize:"2.5em"}}/></a>
+        <img className='slide__img' src={image} alt={num} />
+      </div>
     </div>
   );
 }
--- src/containers/ModalProject.jsx
+++ src/containers/ModalProject.jsx
@@ -1,28 +1,74 @@
-import React from 'react';
-import ProjectSlide from '../components/ProjectSlide';
-import TitleSection from '../components/TitleSection';
+import React from "react";
+import ProjectSlide from "../components/ProjectSlide";
+import TitleSection from "../components/TitleSection";
+import web_projects from "@images/web-projects.png";
 
-function ModalProject({title, subtitle}) {
-  function closeModal(title){
+import Friche from "@images/desktop/FricheAdminApp.png";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import Yard from "@images/desktop/YardSale.png";
+
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
+import Todo from "@images/desktop/TodoApp.png";
+
+import GifBoxIcon from '@mui/icons-material/GifBox';
+import GifExpert from "@images/desktop/GifExpert.png";
+
+function ModalProject({ title, subtitle }) {
+  function closeModal() {
     let modal = document.querySelector(".modal");
     let body = document.querySelector("body");
-    modal.style.animation = "disappear-animation 0.5s"
+    modal.style.animation = "disappear-animation 0.5s";
     setTimeout(() => {
       modal.style.display = "none";
       body.style.height = "auto";
       body.style.overflow = "auto";
-      modal.style.animation = "appear-animation 0.5s"
+      modal.style.animation = "appear-animation 0.5s";
     }, 500);
   }
   return (
     <>
-      <section className='modal'>
+      <section className="modal">
         <TitleSection title={title} subtitle={subtitle} />
-        <button className='modal__close' onClick={()=>closeModal(title)}>X</button>
-        <ProjectSlide num="1"/>
-        <ProjectSlide num="2"/>
-        <ProjectSlide num="3"/>
-        <ProjectSlide num="4"/>
+        <button className="modal__close" onClick={() => closeModal()}>
+          X
+        </button>
+        <ProjectSlide
+          num="1"
+          image={Friche}
+          icon={<DashboardIcon className="slide__icon" />}
+          title={"Friche Admin App"}
+          desc={"Administration page for a candy grocery store"}
+          link={"https://epsyloun.github.io/FricheAdminApp/"}
+          git={"https://github.com/Epsyloun/FricheAdminApp"}
+        />
+        <ProjectSlide
+          num="2"
+          image={Yard}
+          icon={<MonetizationOnIcon className="slide__icon" />}
+          title={"Yard Sale"}
+          desc={"Administration page for a yard store"}
+          link={"https://epsyloun.github.io/react-shop/"}
+          git={"https://github.com/Epsyloun/react-shop"}
+        />
+        <ProjectSlide
+          num="3"
+          image={Todo}
+          icon={<TaskAltIcon className="slide__icon" />}
+          title={"Todo App"}
+          link={"https://epsyloun.github.io/todo-machine/"}
+          git={"https://github.com/Epsyloun/todo-machine"}
+        />
+        <ProjectSlide
+          num="4"
+          image={GifExpert}
+          icon={<GifBoxIcon className="slide__icon" />}
+          title={"Gif Expert App"}
+          desc={"Gif App that brings gif from an API"}
+          link={"https://epsyloun.github.io/react-gifexpertapp/"}
+          git={"https://github.com/Epsyloun/react-gifexpertapp"}
+        />
       </section>
     </>
   );
